31ebc332c656a6c1e5e900732be60b97866ca87b
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2021-03-25 16:23:28 +0100

    Add deprecation warning

--- src/xarray_regex/__init__.py
+++ src/xarray_regex/__init__.py
@@ -1,4 +1,6 @@
 
+import warnings
+
 from .file_finder import FileFinder
 
 __version__ = "0.2.1"
@@ -6,3 +8,6 @@ __version__ = "0.2.1"
 __all__ = [
     'FileFinder'
 ]
+
+warnings.warn(("Xarray-regex is now deprecated and has been "
+               "replaced by https://github.com/Descanonge/filefinder"))
